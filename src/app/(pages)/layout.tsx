d0de8925f11fcab98246afc001857cdc3a75384a
huylc12343 <huylc12343@gmail.com>
2026-03-16 17:58:31 +0700

    Feat: added Analytics to tracking visitors

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,6 +1,6 @@
 import Navbar from "@/src/components/navbar/Navbar";
 import Footer from "@/src/components/footer/Footer";
-
+import { Analytics } from "@vercel/analytics/next"
 export default function PageLayout({
   children,
 }: {
@@ -10,6 +10,7 @@ export default function PageLayout({
     <>
       <div className="flex flex-col min-h-screen">
         <Navbar />
+        <Analytics/>
         <div className="flex-1  w-full mx-auto">{children}</div>
         <Footer />
       </div>
